5c13447b2d11db693eef40d888aa0c6f71e25744
prdpta <pradiptaputramei01@gmail.com>
2025-11-10 19:45:17 +0700

    membuat composable ListData

---
+++ app/src/main/java/com/example/questnavigasitugas_037/view/ListData.kt
@@ -0,0 +1,22 @@
+package com.example.questnavigasitugas_037.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListData(
+    dataList: List<List<String>>,
+    OnBerandaClick: () -> Unit,
+    OnFormulirClick: () -> Unit
+) {
