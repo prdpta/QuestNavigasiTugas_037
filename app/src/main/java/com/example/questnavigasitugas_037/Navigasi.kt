514be98abb34dd02e39f9ef6eea1c77941d3a4f3
prdpta <pradiptaputramei01@gmail.com>
2025-11-10 18:59:14 +0700

    membuat sistem navigasi

---
+++ app/src/main/java/com/example/questnavigasitugas_037/Navigasi.kt
@@ -0,0 +1,28 @@
+package com.example.questnavigasitugas_037
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_037.view.Formulir
+import com.example.questnavigasitugas_037.view.Home
+import com.example.questnavigasitugas_037.view.ListData
+
+enum class Navigasi {
+    Home,
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
